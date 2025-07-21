ee37a2b94e6c4a552047b51bcc4521fc626c99ec
faijan pathan <official.jaan07@gmail.com>
2025-07-21 13:17:38 +0530

    conditional operator

---
+++ q3.1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    (n%2 ==0) ? printf("even number\n") : printf("odd number\n");
+    return 0;
+}
