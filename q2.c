1c90f6bb8292d38df31726811790a5c8769fb18c
faijan pathan <official.jaan07@gmail.com>
2025-07-21 12:42:03 +0530

    question 2

---
+++ q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n>0)
+    printf("positive number:");
+    else
+    printf("negative number:");
+ return 0;
+}
