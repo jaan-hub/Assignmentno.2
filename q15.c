1eb7cbf932f31fd10ffb38d0a588a10d72072803
faijan pathan <official.jaan07@gmail.com>
2025-07-22 18:54:25 +0530

    salary

---
+++ q15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    float salary, tax;
+    printf("Enter annual basic salary: ");
+    scanf("%f", &salary);
+    if (salary < 150000) {
+        tax = 0;
+    } else if (salary>=15000 && salary<= 300000) {
+        tax = 0.20 * salary;
+    } else {
+        tax = 0.30 * salary;
+    }
+    printf("Income Tax:%.2f\n", tax);
+    return 0;
+}
